9d4e99cb41f2be1be3934ccc1eafe84a1dc4a1e3
jimmyking <jimmyking329@qq.com>
2014-10-21 17:31:11 +0800

    init commit

---
+++ app/main/views.py
@@ -0,0 +1,8 @@
+from flask import render_template, session, redirect, url_for, current_app
+from . import main
+#from .. import db
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('layout.html')
