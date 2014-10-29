33564ac762edb86a5691e57b7d946a4bc9b4e345
jimmyking <jimmyking329@qq.com>
2014-10-29 16:01:52 +0800

    完成导出功能

--- manage.py
+++ manage.py
@@ -5,9 +5,12 @@ from app.models import User, Role, Order, OrderStatus, OrderGroup
 from flask.ext.script import Manager, Shell
 from flask.ext.migrate import Migrate, MigrateCommand
 
+
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
+
+
 
 @app.before_request
 def before_request():
@@ -15,7 +18,8 @@ def before_request():
 
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Order=Order, OrderStatus=OrderStatus, OrderGroup=OrderGroup)
+    return dict(app=app, db=db, User=User, Role=Role, Order=Order, OrderStatus=OrderStatus,\
+                OrderGroup=OrderGroup)
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
 
