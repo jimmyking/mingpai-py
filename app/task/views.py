251018f9ac5176d72214fa8375087c6be5aadb10
jimmyking <jimmyking329@qq.com>
2014-11-04 17:09:04 +0800

    任务处理增加导出

--- app/task/views.py
+++ app/task/views.py
@@ -1,14 +1,17 @@
 #coding=utf-8
 from flask import render_template, session, redirect, url_for, current_app
 from flask import jsonify
-from flask import request
+from flask import request,Response
 from flask.ext.login import login_required,current_user
 from ..models import Order,Area,OrderProcess,OrderStatus,OrderGroup,OrderGroupProcess
 from ..models import GroupTask,OrderTask,OrderTeam,IssueType,WarningType
 from datetime import datetime
 from .. import db
 from . import task
-
+import xlwt
+import StringIO
+import mimetypes
+from werkzeug.datastructures import Headers
 
 @task.route('/')
 @login_required
@@ -184,3 +187,76 @@ def get_available_team(type):
 	teams =  OrderTeam.query.filter_by(group_type=type).filter(OrderTeam.status_id.in_(status)).all()
 	return render_template('team/teams.html',teams=teams)
 
+
+@task.route('/export')
+def export():
+	tid = request.args.get('tid')
+	orders = Order.query.filter_by(team_id=tid).all()
+	response=Response()
+	response.status_code=200
+	workbook=xlwt.Workbook()
+	sheet = workbook.add_sheet('A Test Sheet')
+	sheet.write(0,0,u'单号')
+	sheet.write(0,1,u'类型')
+	sheet.write(0,2,u'状态')
+	sheet.write(0,3,u'区服')
+	sheet.write(0,4,u'角色')
+	sheet.write(0,5,u'账号')
+	sheet.write(0,6,u'密码')
+	sheet.write(0,7,u'起点')
+	sheet.write(0,8,u'需求')
+	sheet.write(0,9,u'当前')
+	sheet.write(0,10,u'旺旺')
+	sheet.write(0,11,u'QQ号')
+	sheet.write(0,12,u'电话')
+	sheet.write(0,13,u'金额')
+	sheet.write(0,14,u'支付')
+	sheet.write(0,15,u'创建时间')
+
+	index = 1;
+	for order in orders:
+		sheet.write(index,0,order.order_no)
+		sheet.write(index,1,order.order_type.name)
+		sheet.write(index,2,order.order_status.name)
+		sheet.write(index,3,order.area.name)
+		sheet.write(index,4,order.acter_name)
+		sheet.write(index,5,order.acter_account)
+		sheet.write(index,6,order.acter_password)
+		sheet.write(index,7,order.start_level)
+		sheet.write(index,8,order.end_level)
+		sheet.write(index,9,order.now_level)
+		sheet.write(index,10,order.wangwang)
+		sheet.write(index,11,order.qq)
+		sheet.write(index,12,order.mobile)
+		sheet.write(index,13,order.amount)
+		sheet.write(index,14,order.paytype)
+		sheet.write(index,15,str(order.create_date))
+		index = index+1;
+
+
+	output = StringIO.StringIO()
+	workbook.save(output)
+	response.data = output.getvalue()
+
+	filename = 'export.xls'
+	mimetype_tuple = mimetypes.guess_type(filename)
+
+	response_headers = Headers({
+			'Pragma' : "public",
+			'Expires' : '0',
+			'Cache-Control': 'must-revalidate, post-check=0, pre-check=0',
+			'Cache-Control': 'private',
+			'Content-Type': mimetype_tuple[0],
+			'Content-Disposition': 'attachment; filename=\"%s\";' % filename,
+			'Content-Transfer-Encoding': 'binary',
+			'Content-Length': len(response.data)
+		})
+
+	if not mimetype_tuple[1] is None:
+		response.update({
+				'Content-Encoding' : mimetype_tuple[1]
+			})
+
+	response.headers = response_headers
+
+	return response
